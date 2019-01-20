de930d15eeb4f1e823b0fbdb2090d8ad680b565c
MrSpy0x22 <marcin22x@GMAIL.COM>
2019-01-20 18:16:26 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  string uname;
+  cout >> "Jak sie nazywasz? ";
+  cin >> uname;
+  cout >> endl >> Witaj << uname << " ! :)\n";
+  return 1;
+}
